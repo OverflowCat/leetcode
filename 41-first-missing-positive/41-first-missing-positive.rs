4ca3d770848ee730d94565bb7576c7c1cf4c1f13
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-07-12 17:22:03 +0800

    Time: 84 ms (11.59%), Space: 13.3 MB (8.70%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.rs
+++ 41-first-missing-positive/41-first-missing-positive.rs
@@ -7,9 +7,9 @@ fn hashset(data: &[i32]) -> HashSet<i32> {
 
 impl Solution {
     pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
-        let set = hashset(&nums);
+        let set: HashSet<&i32> = HashSet::from_iter(nums.iter());
         let len = nums.len() as i32;
-        for i in 1..=len { // 如果是 len + 1 的话，nums 一定是从 1 到 len 的连续正整数
+        for i in 1..=len {
             if !set.contains(&i) {
                 return i;
             }
