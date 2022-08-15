9bcb926053979fd56a4fee8feb2106044f030f51
猫猫 <overflowcat@gmail.com>
2022-08-15 15:46:45 +0800

    First working version

--- rust/construct-smallest-number-from-di-string/src/main.rs
+++ rust/construct-smallest-number-from-di-string/src/main.rs
@@ -2,23 +2,57 @@ fn main() {
     println!("{}", smallest_number("IIIDIDDD".to_string()));
 }
 
-fn smallest_number(pattern: String) -> String {
+fn smallest_number(mut pattern: String) -> String {
+    pattern.push('X');
     let length = pattern.len();
-    let minimum: u32 = 10u32.pow(length as u32);
-    let maximum = 10u32.pow(length as u32 + 1) - 1;
-    let ops = pattern
-        .chars()
-        .map(|c| match c {
-            'I' => true,
-            _ => false,
-        })
-        .collect::<Vec<bool>>();
-    for i in minimum..=maximum {
-        if is_match(i, &ops) {
-            return i.to_string();
+    let mut res: Vec<u8> = (1..=length as u8).collect();
+    let partial_rev = |v: &mut Vec<u8>, mut i: usize, mut j: usize| {
+        while i < j {
+            v.swap(i, j);
+            i += 1;
+            j -= 1;
+        }
+    };
+    let mut d_start: Option<usize> = None;
+    for (i, b) in pattern.bytes().enumerate() {
+        match b {
+            b'D' => {
+                if d_start.is_none() {
+                    d_start = Some(i);
+                }
+            }
+            _ => {
+                if d_start.is_some() {
+                    partial_rev(&mut res, d_start.unwrap(), i);
+                    d_start = None;
+                }
+            }
         }
     }
-    return "".to_string();
+    res.iter().map(|x| x.to_string()).collect()
 }
 
+/* fn smallest_number(pattern: String) -> String {
+    let mut is_used = [false; 10];
+    let mut result: [u8; 9];
+    let mut top = 0usize;
+    pattern.bytes().for_each(|b| {
+        let min_not_used = {
+            let mut min = 0;
+            for i in b + 1..10 {
+                if !is_used[i] {
+                    min = i;
+                    break;
+                }
+            }
+            if b == 0 {
+
+            }
+            min
+        };
+
+    });
+
+}
+ */
 // fn is_match(number: u32, ops: &Vec<bool>) -> bool {}
