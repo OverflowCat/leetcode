113ef1c3cd49fce640fa4700f101dd723e8aaf82
猫猫 <overflowcat@gmail.com>
2022-08-19 04:33:26 +0800

    Rob II AC

--- rust/house-robber-ii/src/main.rs
+++ rust/house-robber-ii/src/main.rs
@@ -8,29 +8,46 @@ pub fn main() {
     let res = Solution::rob(vec![1, 2, 3]);
     assert!(3 == res, "Expected 3, got {}", res);
     println!("{}", res);
+
+    /*
+    73 / 75 test cases passed.
+    Input:
+    [1]
+    Output:
+    0
+    Expected:
+    1
+    */
+    let res = Solution::rob(vec![1i32]);
+    assert!(1 == res, "Expected 1, got {}", res);
+    println!("{}", res);
 }
 
 struct Solution {}
 
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        {
-            let (mut i, mut i1, mut i2) = (0, 0, 0);
-            for j in (1..nums.len()).rev() {
-                i = i1.max(i2 + nums[j]);
-                i2 = i1;
-                i1 = i;
+        match nums.len() {
+            1 => nums[0],
+            2 => nums[0].max(nums[1]),
+            x => {
+                let (mut i, mut i1, mut i2) = (0, 0, 0);
+                for j in (1..x).rev() {
+                    i = i1.max(i2 + nums[j]);
+                    i2 = i1;
+                    i1 = i;
+                }
+                i
             }
-            i
+            .max({
+                let (mut i, mut i1, mut i2) = (0, 0, 0);
+                for j in (0..x - 1).rev() {
+                    i = i1.max(i2 + nums[j]);
+                    i2 = i1;
+                    i1 = i;
+                }
+                i
+            }),
         }
-        .max({
-            let (mut i, mut i1, mut i2) = (0, 0, 0);
-            for j in (0..nums.len() - 1).rev() {
-                i = i1.max(i2 + nums[j]);
-                i2 = i1;
-                i1 = i;
-            }
-            i
-        })
     }
 }
