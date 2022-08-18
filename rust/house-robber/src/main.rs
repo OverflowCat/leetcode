9ecfa5c273388ff03bb64c71de181df984784251
猫猫 <overflowcat@gmail.com>
2022-08-19 03:56:09 +0800

    Rob 1

--- rust/house-robber/src/main.rs
+++ rust/house-robber/src/main.rs
@@ -1,9 +1,9 @@
 pub fn main() {
-    let res = rob(&vec![2, 7, 9, 3, 1], 0);
+    let res = Solution::rob(vec![2i32, 7, 9, 3, 1]);
     println!("{}", res);
-    let res = rob(&vec![1, 2, 3, 1], 0);
+    let res = Solution::rob(vec![1, 2i32, 3, 1]);
     println!("{}", res);
-    let res = rob(&vec![2, 1, 1, 2], 0);
+    let res = Solution::rob(vec![2i32, 1, 1, 2]);
     println!("{}", res);
     /*
     59 / 68 test cases passed.
@@ -12,13 +12,29 @@ pub fn main() {
     Output:
     3
     Expected:
+
     4
     */
 }
 
-pub fn dp(nums: &Vec<i32>, start: usize) -> i32 {
-    if start >= nums.len() {
-        return 0;
+struct Solution {}
+
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        let memo = &mut Box::new(vec![-1; nums.len()]);
+        Self::dp(&nums, 0, memo)
     }
-    return dp(nums, start + 1).max(dp(nums, start + 2) + nums[start]);
+
+    pub fn dp(nums: &Vec<i32>, start: usize, memo: &mut Box<Vec<i32>>) -> i32 {
+        if start >= nums.len() {
+            0
+        } else if memo[start] != -1 {
+            memo[start]
+        } else {
+            let res =
+                Self::dp(nums, start + 1, memo).max(Self::dp(nums, start + 2, memo) + nums[start]);
+            memo[start] = res;
+            res
+        }
+    }
 }
