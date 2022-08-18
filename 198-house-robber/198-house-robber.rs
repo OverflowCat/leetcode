73be2ced9b97c3d94cdf079bde511837a4aa9c08
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-08-19 04:02:37 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (29.90%) - LeetHub

--- 198-house-robber/198-house-robber.rs
+++ 198-house-robber/198-house-robber.rs
@@ -1,19 +1,11 @@
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        let memo = &mut Box::new(vec![-1; nums.len()]);
-        Self::dp(&nums, 0, memo)
-    }
-
-    pub fn dp(nums: &Vec<i32>, start: usize, memo: &mut Box<Vec<i32>>) -> i32 {
-        if start >= nums.len() {
-            0
-        } else if memo[start] != -1 {
-            memo[start]
-        } else {
-            let res =
-                Self::dp(nums, start + 1, memo).max(Self::dp(nums, start + 2, memo) + nums[start]);
-            memo[start] = res;
-            res
-        }
+        let (mut i, mut i1, mut i2) = (0, 0, 0);
+        nums.into_iter().rev().for_each(|x| {
+            i = i1.max(i2 + x);
+            i2 = i1;
+            i1 = i;
+        });
+        i
     }
 }
