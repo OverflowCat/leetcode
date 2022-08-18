8595e4de21b5336b0da05914efad35d31f41b336
猫猫 <overflowcat@gmail.com>
2022-08-19 04:30:57 +0800

    Rob II

--- rust/house-robber/src/main.rs
+++ rust/house-robber/src/main.rs
@@ -22,11 +22,11 @@ struct Solution {}
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
         let (mut i, mut i1, mut i2) = (0, 0, 0);
-        nums.into_iter().rev().for_each(|x| {
-            i = i1.max(i2 + x);
+        for j in (0..nums.len()).rev() {
+            i = i1.max(i2 + nums[j]);
             i2 = i1;
             i1 = i;
-        });
+        }
         i
     }
 }
