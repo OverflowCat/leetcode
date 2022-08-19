e6a9bca635089123f4e797489b057a98b958a2fd
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-08-20 02:19:41 +0800

    Time: 14 ms (38.96%), Space: 2.2 MB (39.61%) - LeetHub

--- 55-jump-game/55-jump-game.rs
+++ 55-jump-game/55-jump-game.rs
@@ -1,17 +1,13 @@
 impl Solution {
     pub fn can_jump(nums: Vec<i32>) -> bool {
         let length = nums.len();
-        let mut can = vec![false; length];
-        can[length - 1] = true;
-        for i in (0..length).rev() {
-            for j in (i + 1)..((i + nums[i] as usize + 1).min(length)) {
-                // println!("i: {}, j: {}, can[j]: {}", i, j, can[j]);
-                if can[j] {
-                    can[i] = true;
-                    break;
-                }
+        let mut min_accessible = length - 1;
+        nums.into_iter().enumerate().rev().for_each(|(i, num)| {
+            // println!("{} {}", i, num);
+            if i + num as usize >= min_accessible {
+                min_accessible = i;
             }
-        }
-        can[0]
+        });
+        min_accessible == 0
     }
 }
