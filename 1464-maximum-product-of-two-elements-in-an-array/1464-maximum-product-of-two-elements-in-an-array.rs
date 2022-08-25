21733ad4adb1cde356094f9bd8e37d93ce26e7a8
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-08-26 01:28:52 +0800

    Time: 1 ms (76.47%), Space: 2.3 MB (5.88%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.rs
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.rs
@@ -1,7 +1,17 @@
 impl Solution {
     pub fn max_product(nums: Vec<i32>) -> i32 {
-        let mut nums = nums;
-        nums.sort();
-        (nums[nums.len() - 1] - 1) * (nums[nums.len() - 2] - 1)
+        let mut 最大 = 0;
+        let mut 第二大 = 0; // [10,2,5,2]
+        nums.into_iter().for_each(|x| {
+            if x >= 第二大 {
+                if x >= 最大 {
+                    第二大 = 最大;
+                    最大 = x;
+                } else {
+                    第二大 = x;
+                }
+            }
+        });
+        (最大 - 1) * (第二大 - 1)
     }
 }
