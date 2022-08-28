e4b3ef94b5ae08f5db8379e5888d55efa5bfcab6
猫猫 <overflowcat@gmail.com>
2022-08-28 10:41:16 +0800

    using iter

--- rust/sort-the-matrix-diagonally/src/main.rs
+++ rust/sort-the-matrix-diagonally/src/main.rs
@@ -1,3 +1,5 @@
+struct Solution {}
+
 fn print_matrix(mat: &Vec<Vec<i32>>) {
     for line in mat {
         for item in line {
@@ -38,64 +40,33 @@ fn main() {
         ]
     );
 
-    let mat = vec![vec![99]];
+    let mat = vec![vec![-99]];
+    print_matrix(&mat);
     let res = Solution::diagonal_sort(mat);
-    assert_eq!(res, vec![vec![99]]);
+    print_matrix(&res);
+    assert_eq!(res, vec![vec![-99]]);
 }
-
-struct Solution {}
 
 impl Solution {
     pub fn diagonal_sort(mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let m = mat.len();
-        let n = mat[0].len();
+        let (m, n) = (mat.len(), mat[0].len());
         let mut mat = mat;
         for x in 0..n {
-            let (mut i, mut j) = (m - 1, x);
-            let mut this_line: Vec<i32> = vec![];
-            loop {
-                this_line.push(mat[i][j]);
-                if i == 0 || j == 0 {
-                    break;
-                }
-                i -= 1;
-                j -= 1;
-            }
-            this_line.sort();
-            // this_line.reverse();
-
-            let (mut i, mut j) = (m - 1, x);
-            loop {
-                mat[i][j] = this_line.pop().unwrap();
-                if i == 0 || j == 0 {
-                    break;
-                }
-                i -= 1;
-                j -= 1;
+            let mut this_line: Vec<i32> = (0..=x.min(m - 1))
+                .map(|i| mat[m - 1 - i][x - i])
+                .collect();
+            this_line.sort_unstable();
+            for i in 0..=x.min(m - 1) {
+                mat[m - 1 - i][x - i] = this_line.pop().unwrap();
             }
         }
-
         for y in (0..m - 1).rev() {
-            let (mut i, mut j) = (y, n - 1);
-            let mut this_line: Vec<i32> = vec![];
-            loop {
-                this_line.push(mat[i][j]);
-                if i == 0 || j == 0 {
-                    break;
-                }
-                i -= 1;
-                j -= 1;
-            }
-            this_line.sort();
-            // println!("This line is {:?}", this_line);
-            let (mut i, mut j) = (y, n - 1);
-            loop {
-                mat[i][j] = this_line.pop().unwrap();
-                if i == 0 || j == 0 {
-                    break;
-                }
-                i -= 1;
-                j -= 1;
+            let mut this_line: Vec<i32> = (0..=y.min(n - 1))
+                .map(|i| mat[y - i][n - 1 - i])
+                .collect();
+            this_line.sort_unstable();
+            for i in 0..=y.min(n - 1) {
+                mat[y - i][n - 1 - i] = this_line.pop().unwrap();
             }
         }
         mat
