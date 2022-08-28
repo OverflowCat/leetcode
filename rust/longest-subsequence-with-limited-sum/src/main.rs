54530da75dd4f6ceae621c7ea9af4e3917f73bf5
猫猫 <overflowcat@gmail.com>
2022-08-28 12:41:41 +0800

    binary_search

--- rust/longest-subsequence-with-limited-sum/src/main.rs
+++ rust/longest-subsequence-with-limited-sum/src/main.rs
@@ -1,26 +1,22 @@
 impl Solution {
     pub fn answer_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
         let mut nums = nums;
-        let length = nums.len();
         nums.sort_unstable();
         let mut sum = 0;
-        let mut nums_i = 0;
+        let sums: Vec<i32> = nums
+            .iter()
+            .map(|&x| {
+                sum += x;
+                sum
+            })
+            .collect();
         queries
             .iter()
-            .map(|&q| {
-                if nums_i == length - 1 {
-                    nums_i as i32 + 1
-                } else {
-                    while sum <= q && nums_i < length {
-                        sum += nums[nums_i];
-                        nums_i += 1;
-                    }
-                    if sum > q {
-                        nums_i as i32 - 1
-                    } else {
-                        nums_i as i32
-                    }
-                }
+            .map(|q| {
+                (match sums.binary_search(&(*q + 1)) {
+                    Ok(x) => x - 1,
+                    Err(x) => x,
+                }) as i32
             })
             .collect()
     }
@@ -32,5 +28,9 @@ fn main() {
     println!(
         "{:?}",
         Solution::answer_queries(vec![4, 5, 2, 1], vec![3, 10, 21])
+    );
+    println!(
+        "{:?}",
+        Solution::answer_queries(vec![3,4,5,2], vec![1])
     );
 }
