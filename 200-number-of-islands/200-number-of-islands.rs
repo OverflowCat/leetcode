496aaade944b4397702bb55de097753b054d23bb
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-08-29 11:05:52 +0800

    Time: 18 ms (67.01%), Space: 9.1 MB (28.93%) - LeetHub (+1 squashed commits)
    Squashed commits:
    
    [ceff66f] Attach NOTES - LeetHub

--- 200-number-of-islands/200-number-of-islands.rs
+++ 200-number-of-islands/200-number-of-islands.rs
@@ -1,32 +1,30 @@
 impl Solution {
-    pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
-        let mmax = grid.len() - 1;
-        let nmax = grid[0].len() - 1;
+    pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
+        let n = grid[0].len();
         let mut counter = 0;
-        let mut grid = grid;
-        for i in 0..=mmax {
-            for j in 0..=nmax {
+        for i in 0..grid.len() {
+            for j in 0..n {
                 if grid[i][j] == '1' {
                     counter += 1;
-                    dfs(i, j, &mut grid, mmax, nmax);
+                    dfs(i, j, &mut grid);
                 }
             }
         }
 
-        fn dfs(i: usize, j: usize, grid: &mut Vec<Vec<char>>, mmax: usize, nmax: usize) {
+        fn dfs(i: usize, j: usize, grid: &mut Vec<Vec<char>>) {
             if grid[i][j] == '1' {
                 grid[i][j] = '2';
                 if i > 0 {
-                    dfs(i - 1, j, grid, mmax, nmax);
+                    dfs(i - 1, j, grid);
                 }
                 if j > 0 {
-                    dfs(i, j - 1, grid, mmax, nmax);
+                    dfs(i, j - 1, grid);
                 }
-                if i < mmax {
-                    dfs(i + 1, j, grid, mmax, nmax);
+                if i < grid.len() - 1 {
+                    dfs(i + 1, j, grid);
                 }
-                if j < nmax {
-                    dfs(i, j + 1, grid, mmax, nmax);
+                if j < grid[0].len() - 1 {
+                    dfs(i, j + 1, grid);
                 }
             }
         }
