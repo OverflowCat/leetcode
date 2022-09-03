61aa99770b601881b7f73afbe8301685a547386b
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-09-04 00:15:10 +0800

    Time: 2 ms (100.00%), Space: 1.9 MB (100.00%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn is_strictly_palindromic(n: i32) -> bool {
+        (2..n - 1).all(|base| {
+            let mut res: Vec<i32> = Vec::new();
+            let mut n = n;
+            while n != 0 {
+                res.push(n % base);
+                n /= base;
+            }
+            res.iter()
+                .rev()
+                .zip(res.iter())
+                .take(res.len() / 2 + 1)
+                .all(|(&f, &b)| f == b)
+        })
+    }
+}
