139d410b5b07ec85188dea5fec672da3d30da72b
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-09-03 11:07:43 +0800

    Time: 0 ms (100.00%), Space: 2 MB (100.00%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.rs
@@ -0,0 +1,35 @@
+impl Solution {
+    pub fn nums_same_consec_diff(n: i32, k: i32) -> Vec<i32> {
+        if k == 0 {
+            (1..10)
+                .map(|d| {
+                    let mut r = 0;
+                    for _ in 0..n {
+                        r = r * 10 + d;
+                    }
+                    r
+                })
+                .collect()
+        } else {
+            fn f(gen: i32, last: i32, left: i32, res: &mut Vec<i32>, k: &i32) {
+                if left == 0 {
+                    res.push(gen);
+                    return;
+                }
+                let next = last + k;
+                if next < 10 {
+                    f(gen * 10 + next, next, left - 1, res, k);
+                }
+                let next = last - k;
+                if next > -1 {
+                    f(gen * 10 + next, next, left - 1, res, k);
+                }
+            }
+            let mut res: Vec<i32> = vec![];
+            for i in 1..10 {
+                f(i, i, n - 1, &mut res, &k);
+            }
+            res
+        }
+    }
+}
