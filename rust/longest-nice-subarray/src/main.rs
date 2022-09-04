796c4139840b9f6db3fefdc0633717f3d913f2ee
猫猫 <overflowcat@gmail.com>
2022-09-04 12:48:13 +0800

    attempts

--- rust/longest-nice-subarray/src/main.rs
+++ rust/longest-nice-subarray/src/main.rs
@@ -1,20 +1,16 @@
 impl Solution {
     pub fn longest_nice_subarray(nums: Vec<i32>) -> i32 {
-        let (mut count, mut max_count) = (0usize, 0usize);
-        let v = nums
-            .windows(2)
-            .map(|slice| slice[0] & slice[1] == 0)
-            .collect::<Vec<_>>();
-        println!("{:?}", v);
-
-        v.into_iter().for_each(|b| match b {
-            true => count += 1,
-            false => {
-                max_count = max_count.max(count);
-                count = 0;
+        let (mut left, mut max_len) = (0usize, 0usize);
+        for i in 1..nums.len() {
+            for j in left..i {
+                if nums[j] & nums[i] != 0 {
+                    max_len = max_len.max(i - left);
+                    left = j + 1;
+                    break;
+                }
             }
-        });
-        max_count.max(count).max(1) as i32
+        }
+        max_len.max(nums.len() - 1 - left) as i32
     }
 }
 
@@ -22,10 +18,19 @@ struct Solution {}
 
 fn main() {
     let nums = vec![1, 3, 8, 48, 10];
-    Solution::longest_nice_subarray(nums);
+    let res = Solution::longest_nice_subarray(nums);
+    println!("{}", res);
     let nums = vec![
         744437702, 379056602, 145555074, 392756761, 560864007, 934981918, 113312475, 1090, 16384,
         33, 217313281, 117883195, 978927664,
     ];
-    Solution::longest_nice_subarray(nums);
+    let res = Solution::longest_nice_subarray(nums);
+    println!("{}", res);
+    let nums = vec![
+        84139415, 693324769, 614626365, 497710833, 615598711, 264, 65552, 50331652, 1, 1048576,
+        16384, 544, 270532608, 151813349, 221976871, 678178917, 845710321, 751376227, 331656525,
+        739558112, 267703680,
+    ];
+    let res = Solution::longest_nice_subarray(nums);
+    println!("{}", res);
 }
