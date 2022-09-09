b3de156880d9ac55974c3b7445c5d4436836fcc3
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-09-09 21:28:49 +0800

    Time: 200 ms (33.33%), Space: 11.6 MB (33.33%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn number_of_weak_characters(mut properties: Vec<Vec<i32>>) -> i32 {
+        properties.sort_unstable_by(|a, b| match b[0].cmp(&a[0]) {
+            std::cmp::Ordering::Equal => a[1].cmp(&b[1]),
+            other => other,
+        });
+        /* 3 6, 5 5, 6 3 */
+        let mut max_def = 0;
+        properties
+            .iter()
+            .map(|x| {
+                if x[1] < max_def {
+                    1
+                } else {
+                    max_def = x[1];
+                    0
+                }
+            })
+            .sum()
+    }
+}
