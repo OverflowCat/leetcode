31f2fa47596b6e4646eda61c6812a9813edea909
猫猫 <overflowcat@gmail.com>
2022-09-11 01:44:56 +0800

    INF

--- rust/best-time-to-buy-and-sell-stock-iv/src/main.rs
+++ rust/best-time-to-buy-and-sell-stock-iv/src/main.rs
@@ -1,28 +1,32 @@
-fn main() {
-    println!("Hello, world!");
-}
-
-struct Solution {}
-
-pub fn max_profit(k: i32, prices: Vec<i32>) -> i32 {
-    let n = prices.len();
-    let k = k as usize;
-    match n {
-        _ if n <= 1 => 0,
-        n if n >= n / 2 => prices
-            .windows(2)
-            .map(|x| if x[1] > x[0] { x[1] - x[0] } else { 0 })
-            .sum(),
-        n => {
-            let mut dp: Vec<Vec<i32>> = vec![vec![0; n]; k + 1];
-            for i in 1..=k {
-                let mut local_max = dp[i - 1][0] - prices[0];
-                for j in 1..=n {
-                    dp[i][j] = dp[i][j - 1].max(prices[j] + local_max);
-                    local_max = local_max.max(dp[i - 1][j] - prices[j]);
+impl Solution {
+    pub fn max_profit(k: i32, prices: Vec<i32>) -> i32 {
+        let n = prices.len();
+        let k = k as usize;
+        match n {
+            _ if n <= 1 => 0,
+            n if n >= n / 2 => prices
+                .windows(2)
+                .map(|x| if x[1] > x[0] { x[1] - x[0] } else { 0 })
+                .sum(),
+            n => {
+                let k = k.min(n / 2);
+                let buy = vec![-std::f64::INFINITY; k + 1];
+                let sell = vec![-std::f64::INFINITY; k + 1];
+                buy[0] = -prices[0] as f64;
+                sell[0] = 0.;
+                for i in 1..n {
+                    let p = prices[i] as f64;
+                    buy[0] = buy[0].max(sell[0] - p);
+                    for j in 1..k + 1 {
+                        buy[j] = buy[j].max(sell[j] - p);
+                        sell[j] = sell[j].max(buy[j - 1] + p);
+                    }
+                }
+                match sell.iter().into_iter().reduce(f32::max) {
+                    Some(&x) => x,
+                    None => 0,
                 }
             }
-            dp[k][n - 1]
         }
     }
 }
