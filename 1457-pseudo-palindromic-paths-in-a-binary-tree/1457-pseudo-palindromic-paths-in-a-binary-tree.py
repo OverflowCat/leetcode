8875f3c8e2942dadd86e7ec83dcffa63846baea7
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-09-14 12:54:32 +0800

    Time: 866 ms (95.95%), Space: 83.5 MB (94.08%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -1,23 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        global count, res
+        count, res = 0, 0
         def f(node):
-            count[node.val] += 1
+            global count, res
+            count ^= 1 << node.val
             if node.left or node.right:
                 if node.left: f(node.left)
                 if node.right: f(node.right)
             else:
-                奇数count = 0
-                for x in count[1:]:
-                    if x % 2 == 1: 奇数count += 1
-                if 奇数count < 2:
-                    count[0] += 1
-            count[node.val] -= 1
+                if count & count - 1 == 0: res += 1
+            count ^= 1 << node.val
         f(root)
-        return count[0]
+        return res
