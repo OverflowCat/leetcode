856e407ab6aade983725cd585ad5705ca19087cf
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-09-23 20:45:28 +0800

    Time: 28 ms (50.00%), Space: 2 MB (25.00%) - LeetHub (+1 squashed commits)
    Squashed commits:
    
    [2ed57c6] Attach NOTES - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.rs
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.rs
@@ -1,12 +1,7 @@
 impl Solution {
     pub fn concatenated_binary(n: i32) -> i32 {
-        let mut o = 0;
-        for i in 1..=n as u64 {
-            let count = 64 - i.leading_zeros();
-            o <<= count;
-            o += i;
-            o %= 1_000_000_007
-        }
-        o as i32
+        (1..=n as u64).fold(0u64, |o, i| {
+            ((o << 64 - i.leading_zeros()) + i) % 1_000_000_007
+        }) as i32
     }
 }
