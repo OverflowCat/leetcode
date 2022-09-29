4a51e897342a7be927200772954546d9f3db6b51
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-09-29 23:01:44 +0800

    Time: 21 ms (11.54%), Space: 2.4 MB (26.92%) - LeetHub (+1 squashed commits)
    Squashed commits:
    
    [1922d9a] Attach NOTES - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.rs
+++ 658-find-k-closest-elements/658-find-k-closest-elements.rs
@@ -1,9 +1,6 @@
 impl Solution{
     pub fn find_closest_elements(arr: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
         let (mut l, mut r, k) = (0, arr.len() - 1, k as usize);
-        if r == 0 {
-            return arr;
-        }
         while r - l + 1 > k {
             if arr[r] - x < x - arr[l] {
                 l += 1;
