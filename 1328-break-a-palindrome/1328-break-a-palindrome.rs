1713ac10a41b2e3fb2357813fdee28846887769c
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-10-10 21:13:55 +0800

    Time: 1 ms (85.71%), Space: 2.3 MB (28.57%) - LeetHub (+1 squashed commits)
    Squashed commits:
    
    [14fe949] Attach NOTES - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.rs
@@ -0,0 +1,34 @@
+impl Solution {
+    pub fn break_palindrome(mut palindrome: String) -> String {
+        unsafe {
+            let p = palindrome.as_bytes_mut();
+            let mut mid = b'_';
+            match p.len() {
+                0 | 1 => {
+                    return String::new();
+                }
+                x if x % 2 == 1 => {
+                    mid = p[x / 2];
+                    p[x / 2] = b'a';
+                }
+                _ => {}
+            }
+            let mut index: Option<usize> = None;
+            for (i, &x) in p.iter().take(p.len() / 2).enumerate() {
+                if x != b'a' {
+                    index = Some(i);
+                    break;
+                }
+            }
+            if let Some(i) = index {
+                p[i] = b'a';
+            } else {
+                p[p.len() - 1] = b'b';
+            }
+            if mid != b'_' {
+                p[p.len() / 2] = mid;
+            }
+            palindrome
+        }
+    }
+}
