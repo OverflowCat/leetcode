c8602663ed7ca3982504ebf35f13191e84c6e3b3
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-10-25 08:56:11 +0800

    Time: 2 ms (44.44%), Space: 2.1 MB (44.44%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.rs
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.rs
@@ -1,10 +1,7 @@
 impl Solution {
     pub fn array_strings_are_equal(word1: Vec<String>, word2: Vec<String>) -> bool {
-        word1.iter().map(|s| s.len()).sum::<usize>() == word2.iter().map(|s| s.len()).sum::<usize>()
-            && word1
-                .join("")
-                .bytes()
-                .zip(word2.join("").bytes())
-                .all(|(a, b)| a == b)
+        let a = word1.join("");
+        let b = word2.join("");
+        a.len() == b.len() && a.bytes().zip(b.bytes()).all(|(a, b)| a == b)
     }
 }
