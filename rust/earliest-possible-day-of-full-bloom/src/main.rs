a68d9123a5e6fc53e9428954f4f08f974159bf4b
猫猫 <overflowcat@gmail.com>
2022-10-29 21:52:10 +0800

    debug

--- rust/earliest-possible-day-of-full-bloom/src/main.rs
+++ rust/earliest-possible-day-of-full-bloom/src/main.rs
@@ -5,12 +5,13 @@ impl Solution {
             .zip(grow_time.into_iter())
             .collect::<std::collections::BinaryHeap<_>>()
             .into_iter()
-            .fold((0, 0), |(sow, bloom), (plant, grow)| {
-                (sow + plant, bloom.max(sow + grow))
-            })
-            .1
+            .for_each(|x| println!("{:?}", x));
+        2
     }
 }
 
 struct Solution {}
 
+fn main() {
+    let res = Solution::earliest_full_bloom(vec![1, 4, 3,2,2], vec![2, 3, 1,6,5]);
+}
