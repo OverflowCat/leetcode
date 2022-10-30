f92710e4f37befe99dcd33d633447b26221b1bf1
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-10-30 23:20:23 +0800

    Time: 5 ms (100.00%), Space: 2.6 MB (75.00%)

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.rs
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.rs
@@ -2,16 +2,12 @@ use std::collections::{HashSet, VecDeque};
 impl Solution {
     pub fn shortest_path(grid: Vec<Vec<i32>>, k: i32) -> i32 {
         let (l1, l2, k) = (grid.len(), grid[0].len(), k as usize);
-        if (l1, l2) == (1, 1) {
-            return 0;
-        }
-        if k >= l1 + l2 - 3 {
-            return (l1 + l2 - 2) as i32;
-        }
+        if (l1, l2) == (1, 1) { return 0; }
+        if k >= l1 + l2 - 3 { return (l1 + l2 - 2) as i32; }
         let k = k as u16;
         let mut q: VecDeque<_> = VecDeque::new();
         q.push_back((0, 0, 0, 0));
-        let mut visited: HashSet<(usize, usize, u16)> = HashSet::with_capacity(l1 * l2);
+        let mut visited: HashSet<(usize, usize, u16)> = HashSet::new();
         visited.insert((0, 0, 0));
         while !q.is_empty() {
             let (x, y, n, ob) = q.pop_front().unwrap();
@@ -26,18 +22,10 @@ impl Solution {
                     }
                 };
             }
-            if x + 1 < l1 {
-                f!(x + 1, y);
-            }
-            if y + 1 < l2 {
-                f!(x, y + 1);
-            }
-            if x > 0 {
-                f!(x - 1, y);
-            }
-            if y > 0 {
-                f!(x, y - 1);
-            }
+            if x + 1 < l1 { f!(x + 1, y); }
+            if y + 1 < l2 { f!(x, y + 1); }
+            if x > 0      { f!(x - 1, y); }
+            if y > 0      { f!(x, y - 1); }
         }
         -1
     }
