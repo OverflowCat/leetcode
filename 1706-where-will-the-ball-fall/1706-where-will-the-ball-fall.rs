42c3bd44673b96b79519bf965c8946f636220840
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-11-01 21:29:56 +0800

    Time: 3 ms (80.00%), Space: 2.2 MB (42.86%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.rs
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.rs
@@ -1,29 +1,29 @@
 impl Solution {
     pub fn find_ball(grid: Vec<Vec<i32>>) -> Vec<i32> {
         let (m, n) = (grid.len(), grid[0].len());
-        (0..n)
-            .map(|i| {
-                let mut f: Option<i32> = None;
-                let mut x = i;
-                for j in 0..m {
-                    if grid[j][x] == 1 {
-                        // 1　＼
-                        if x == n - 1 || grid[j][x + 1] == -1 {
-                            f = Some(-1);
-                            break;
-                        }
-                        x += 1;
-                    } else {
-                        // -1　／
-                        if x == 0 || grid[j][x - 1] == 1 {
-                            f = Some(-1);
-                            break;
-                        }
-                        x -= 1;
+        let mut res = Vec::with_capacity(n);
+        for i in 0..n {
+            let mut f: Option<i32> = None;
+            let mut x = i;
+            for j in 0..m {
+                if grid[j][x] == 1 {
+                    // 1　＼
+                    if x == n - 1 || grid[j][x + 1] == -1 {
+                        f = Some(-1);
+                        break;
                     }
+                    x += 1;
+                } else {
+                    // -1　／
+                    if x == 0 || grid[j][x - 1] == 1 {
+                        f = Some(-1);
+                        break;
+                    }
+                    x -= 1;
                 }
-                f.unwrap_or(x as i32)
-            })
-            .collect()
+            }
+            res.push(f.unwrap_or(x as i32));
+        }
+        res
     }
 }
