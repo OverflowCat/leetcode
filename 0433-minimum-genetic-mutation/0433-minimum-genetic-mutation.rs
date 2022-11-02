b530b7a4a3850b07667e6b876bb57c552ffec127
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-11-02 23:39:22 +0800

    Time: 1 ms (62.50%), Space: 2.2 MB (25.00%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.rs
@@ -0,0 +1,36 @@
+use std::collections::VecDeque;
+impl Solution {
+    pub fn min_mutation(start: String, end: String, mut bank: Vec<String>) -> i32 {
+        if !bank.contains(&end) { return -1; }
+        let mut q = VecDeque::with_capacity(bank.len());
+        bank.push(end.clone());
+        q.push_back((start, 0));
+        loop {
+            if let Some((s, t)) = q.pop_front() {
+                // println!("{} {}", s, t);
+                if s == end { return t; }
+                let t = t + 1;
+                { //drain filter
+                    let mut i = 0;
+                    while i < bank.len() {
+                        if Solution::diff(&bank[i], &s) {
+                            q.push_back((bank.remove(i), t));
+                        } else { i += 1; }
+                    }
+                }
+            } else { return -1; }
+        }
+    }
+
+    #[inline(always)]
+    pub fn diff(a: &String, b: &String) -> bool {
+        let mut flag = false;
+        for (&x, &y) in a.as_bytes().iter().zip(b.as_bytes().iter()) {
+            if x != y {
+                if flag { return false; }
+                flag = true;
+            }
+        }
+        flag
+    }
+}
