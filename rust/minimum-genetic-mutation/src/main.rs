570baebee725bfd5e34086e2d531e47dac644bca
猫猫 <overflowcat@gmail.com>
2022-11-03 20:09:11 +0800

    longest_palindrome

--- rust/minimum-genetic-mutation/src/main.rs
+++ rust/minimum-genetic-mutation/src/main.rs
@@ -1,43 +1,52 @@
-#![feature(hash_drain_filter)]
-use std::collections::{HashSet, VecDeque};
-struct Solution;
+use std::collections::VecDeque;
 impl Solution {
-    pub fn min_mutation(mut start: String, end: String, bank: Vec<String>) -> i32 {
-        let bank: HashSet<String> = bank.into_iter().collect();
-        if !bank.contains(&end) {
-            return -1;
-        }
-        let mut queue = Vec::with_capacity(bank.len());
-        while start != end {
-            bank.drain_filter(|x| {
-                if Solution::diff(x, &start) {
-                    queue.push(x.clone());
-                    true
-                } else {
-                    false
+    pub fn min_mutation(start: String, end: String, mut bank: Vec<String>) -> i32 {
+        if !bank.contains(&end) { return -1; }
+        let mut q = VecDeque::with_capacity(bank.len());
+        bank.push(end.clone());
+        q.push_back((start, 0));
+        loop {
+            if let Some((s, t)) = q.pop_front() {
+                println!("{} {}", s, t);
+                if s == end { return t; }
+                let t = t + 1;
+                { //drain filter
+                    let mut i = 0;
+                    while i < bank.len() {
+                        if Solution::diff(&bank[i], &s) {
+                            q.push_back((bank.remove(i), t));
+                        } else {
+                            i += 1;
+                        }
+                    }
                 }
-            });
-            let mut new_queue = Vec::with_capacity(bank.len());
-            for x in queue.iter() {
-                if Solution::diff(x, &start) {
-                    
-                }
-            }
+            } else { return -1; }
         }
-        -1
     }
 
     #[inline(always)]
     pub fn diff(a: &String, b: &String) -> bool {
         let mut flag = false;
         for (&x, &y) in a.as_bytes().iter().zip(b.as_bytes().iter()) {
-            if x == y {
-                if flag {
-                    return false;
-                }
+            if x != y {
+                if flag { return false; }
                 flag = true;
             }
         }
         flag
     }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::diff(&"AACCGGTT".into(), &"AACCGGTA".into()), true);
+    let start = "AACCGGTT".into();
+    let end = "AACCGGTA".into();
+    let bank = vec!["AACCGGTA".into()];
+    Solution::min_mutation(start, end, bank);
+    let start = "TTTTTATG".into();
+    let end = "TTTTTTTG".into();
+    let bank = vec!["CCCTTCCC".into()];
+    assert_eq!(Solution::min_mutation(start, end, bank), -1);
 }
