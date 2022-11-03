a43f078c0064a4f2111ca9ec1ef9ffcd73523143
猫猫 <overflowcat@gmail.com>
2022-11-03 21:30:36 +0800

    update

--- rust/longest-palindrome-by-concatenating-two-letter-words/src/main.rs
+++ rust/longest-palindrome-by-concatenating-two-letter-words/src/main.rs
@@ -1,20 +1,53 @@
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(
+        6,
+        Solution::longest_palindrome(vec![
+            "ab".into(),
+            "ty".into(),
+            "yt".into(),
+            "lc".into(),
+            "cl".into(),
+            "ab".into()
+        ])
+    );
 }
 
 struct Solution {}
 
 impl Solution {
     pub fn longest_palindrome(words: Vec<String>) -> i32 {
-        let mut pairs: HashMap<String, usize> = HashMap::with_capacity(words.len() / 2);
+        let mut hm: HashMap<String, usize> = HashMap::with_capacity(words.len());
+        let mut counter = 0;
+        let mut is_middle_not_used = true;
         words.into_iter().for_each(|w| {
             let rev = w.chars().rev().collect::<String>();
-            let a = pairs.entry(w);
-            let b = pairs.entry(rev);
-            
+            if is_middle_not_used && rev == w {
+                counter += w.len();
+                is_middle_not_used = true;
+                return;
+            }
+            let mut flag = 0u8;
+            hm.entry(rev.clone()).and_modify(|count| {
+                if *count == 1 {
+                    flag = 2;
+                } else {
+                    *count -= 1;
+                    flag = 1;
+                }
+                counter += rev.len();
+            });
+            match flag {
+                2 => {
+                    *hm.entry(rev).or_insert(0) += 1;
+                }
+                0 => {
+                    *hm.entry(w).or_insert(0) += 1;
+                }
+                _ => {}
+            }
         });
-        114
+        counter as i32
     }
 }
