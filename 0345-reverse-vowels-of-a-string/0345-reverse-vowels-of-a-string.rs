438c97f0d629e3b931fe5e79484dc70916e0190e
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-11-04 21:17:11 +0800

    Time: 0 ms (100.00%), Space: 2.5 MB (77.78%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.rs
@@ -0,0 +1,39 @@
+impl Solution {
+    pub fn reverse_vowels(mut s: String) -> String {
+        let (mut i, mut j) = (0, s.len() - 1);
+        unsafe {
+            let b = s.as_bytes_mut();
+            while i < j {
+                while i < j {
+                    match b[i] {
+                        b'a' | b'e' | b'i' | b'o' | b'u' | b'A' | b'E' | b'I' | b'O' | b'U' => {
+                            break;
+                        }
+                        _ => {
+                            i += 1;
+                        }
+                    }
+                }
+                while i < j {
+                    match b[j] {
+                        b'a' | b'e' | b'i' | b'o' | b'u' | b'A' | b'E' | b'I' | b'O' | b'U' => {
+                            break;
+                        }
+                        _ => {
+                            j -= 1;
+                        }
+                    }
+                }
+                if i >= j {
+                    break;
+                }
+                if i < j {
+                    b.swap(i, j);
+                    i += 1;
+                    j -= 1;
+                }
+            }
+            s
+        }
+    }
+}
