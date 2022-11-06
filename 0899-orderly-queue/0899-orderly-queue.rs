72f69cfc669a339823ae9ed52811a3f2daa8c6a8
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-11-06 19:52:17 +0800

    Time: 2 ms (100.00%), Space: 2 MB (100.00%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.rs
@@ -0,0 +1,19 @@
+use std::str::from_utf8_unchecked;
+
+impl Solution {
+    pub fn orderly_queue(s: String, k: i32) -> String {
+        let mut s = s.into_bytes();
+        if k == 1 {
+            let mut r = s.clone();
+            for _ in 1..s.len() {
+                r.rotate_left(1);
+                if r < s {
+                    s = r.clone();
+                }
+            }
+        } else {
+            s.sort_unstable();
+        }
+        unsafe { from_utf8_unchecked(&s).to_owned() }
+    }
+}
