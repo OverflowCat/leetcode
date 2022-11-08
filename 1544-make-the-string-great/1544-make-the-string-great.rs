4d36ca95c4c50b027af9a9b0b01b292d4783d2b6
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-11-08 22:11:39 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (50.00%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.rs
@@ -0,0 +1,36 @@
+const DIFF: u8 = b'a' - b'A';
+
+impl Solution {
+    pub fn make_good(mut s: String) -> String {
+        unsafe {
+            let bytes = s.as_bytes_mut();
+            let (mut i, mut j, length) = (1, 1, bytes.len());
+            let mut prev_char = bytes[0];
+            while i < length {
+                // println!("i is {}, bytes[i] is {}, j is {}, bytes[j] is {}, prev is {}", i, bytes[i] as char, j, bytes[j] as char, prev_char as char);
+                if Solution::abs_diff(bytes[i], prev_char) == DIFF {
+                    i += 1;
+                    j -= 1;
+                    if j > 0 {
+                        prev_char = bytes[j - 1];
+                    } else {
+                        prev_char = 3;
+                    }
+                } else {
+                    prev_char = bytes[i];
+                    bytes[j] = prev_char;
+                    i += 1;
+                    j += 1;
+                }
+            }
+            s.get_unchecked_mut(0..j).to_owned()
+        }
+    }
+    fn abs_diff<T: std::ops::Sub<Output = T> + Ord>(x: T, y: T) -> T {
+        if x < y {
+            y - x
+        } else {
+            x - y
+        }
+    }
+}
