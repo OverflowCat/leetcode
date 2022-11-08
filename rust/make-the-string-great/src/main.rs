87b4f99afacd90c4da380985ff4b2f9894e88163
猫猫 <overflowcat@gmail.com>
2022-11-08 22:51:45 +0800

    make_good

--- rust/make-the-string-great/src/main.rs
+++ rust/make-the-string-great/src/main.rs
@@ -4,20 +4,33 @@ impl Solution {
     pub fn make_good(mut s: String) -> String {
         unsafe {
             let bytes = s.as_bytes_mut();
-            let mut j = 1;
-            let mut prev_i = bytes[0];
-            for i in 1..bytes.len() {
-                if bytes[i].abs_diff(prev_i) == DIFF {
+            let (mut i, mut j, length) = (1, 1, bytes.len());
+            let mut prev_char = bytes[0];
+            while i < length {
+                // println!("i is {}, bytes[i] is {}, j is {}, bytes[j] is {}, prev is {}", i, bytes[i] as char, j, bytes[j] as char, prev_char as char);
+                if Solution::abs_diff(bytes[i], prev_char) == DIFF {
+                    i += 1;
                     j -= 1;
+                    if j > 0 {
+                        prev_char = bytes[j - 1];
+                    } else {
+                        prev_char = 3;
+                    }
                 } else {
-                    bytes[j] = bytes[i];
-                    prev_i = bytes[i];
+                    prev_char = bytes[i];
+                    bytes[j] = prev_char;
+                    i += 1;
                     j += 1;
                 }
             }
-            println!("s is {}", s);
-            println!("j is {}", j);
             s.get_unchecked_mut(0..j).to_owned()
+        }
+    }
+    fn abs_diff<T: std::ops::Sub<Output = T> + Ord>(x: T, y: T) -> T {
+        if x < y {
+            y - x
+        } else {
+            x - y
         }
     }
 }
