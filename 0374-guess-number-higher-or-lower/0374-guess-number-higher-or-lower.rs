4a16a87164a741180ac501d1aea01ab942a1073c
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-11-16 19:33:07 +0800

    Time: 1 ms (73.44%), Space: 2.1 MB (29.69%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.rs
@@ -0,0 +1,29 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * unsafe fn guess(num: i32) -> i32 {}
+ */
+
+impl Solution {
+    unsafe fn guessNumber(n: i32) -> i32 {
+        let mut mid = n / 2;
+        let mut upper = n;
+        let mut lower = 1;
+        loop {
+            match guess(mid) {
+                1 => {
+                    lower = mid + 1;
+                    mid += (upper - mid + 1) / 2;
+                }
+                -1 => {
+                    upper = mid - 1;
+                    mid -= (mid - lower + 1) / 2;
+                }
+                _ => return mid,
+            }
+        }
+    }
+}
