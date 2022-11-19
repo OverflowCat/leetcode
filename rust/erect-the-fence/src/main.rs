e99dc680246464657220e05c313767e33d412864
猫猫 <overflowcat@gmail.com>
2022-11-19 20:32:12 +0800

    outer_trees

--- rust/erect-the-fence/src/main.rs
+++ rust/erect-the-fence/src/main.rs
@@ -1,10 +1,9 @@
-use std::cmp::Ordering::*;
-#[derive(Copy, Clone, Debug)]
+use std::{cmp::Ordering::*, collections::HashSet};
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 struct Tree(i32, i32);
 impl Solution {
     #[inline]
     fn cross_product(x_1: i32, y_1: i32, x_2: i32, y_2: i32) -> i32 {
-        println!("{}, {}, {}, {}", x_1, y_1, x_2, y_2);
         x_1 * y_2 - x_2 * y_1
     }
 
@@ -20,46 +19,79 @@ impl Solution {
             }
             _ => {}
         });
-        println!("左下角的点是 {:?}", init);
+        // println!("左下角的点是 {:?}", init);
         let mut trees: Vec<Tree> = trees
             .into_iter()
             .map(|t| Tree(t[0] - init.0, t[1] - init.1))
             .collect();
         trees.sort_by(
-            |a, b| match { Self::cross_product(a.0, a.1, b.0, b.1).cmp(&0) } {
+            |a, b| match { 0.cmp(&Self::cross_product(a.0, a.1, b.0, b.1)) } {
                 Equal => (a.0.pow(2) + a.1.pow(2)).cmp(&(b.0.pow(2) + b.1.pow(2))),
                 res => res,
             },
         );
-        println!("Sorted trees is {:?}", trees);
+        // println!("Sorted trees is {:?}", trees);
         let mut stack = Vec::new();
         stack.push(trees[0]);
         stack.push(trees[1]);
-        for t in trees.into_iter().skip(2) {
+        for t in trees.iter().skip(2) {
             while {
                 let length = stack.len();
                 length > 1 && {
                     let (a, b) = (stack[stack.len() - 2], stack[stack.len() - 1]);
-                    println!("a {:?}, b {:?}", a, b);
+                    // println!("a {:?}, b {:?}", a, b);
                     let x_1 = t.0 - a.0;
                     let y_1 = t.1 - a.1;
                     let x_2 = b.0 - a.0;
                     let y_2 = b.1 - a.1;
-                    println!("cp: {}", Self::cross_product(x_1, y_1, x_2, y_2));
-                    Self::cross_product(x_1, y_1, x_2, y_2) < 0
+                    // println!("cp: {}", Self::cross_product(x_1, y_1, x_2, y_2));
+                    Self::cross_product(x_1, y_1, x_2, y_2) > 0
                 }
             } {
-                println!("{:?} poped!", stack.pop().unwrap());
+                stack.pop().unwrap(); // println!("{:?} poped!", stack.pop().unwrap());
             }
-            println!("{:?} pushed!", t);
-            stack.push(t);
+            // println!("{:?} pushed!", t);
+            stack.push(t.clone());
             /*
             ①若结果大于0，则AC在AB的逆时针方向
             ②若结果小于0，则AC在AB的顺时针方向
             ③若结果等于0，则AC与AB平行（其实是共线，但是可能朝向相反） */
         }
+        // println!("stack: {:?}", stack);
+        let mut trees: HashSet<_> = HashSet::from_iter(trees.into_iter());
+        for x in &stack {
+            trees.remove(x);
+        }
+        // println!("trees: {:?}", trees);
+        let mut trees: Vec<_> = trees.into_iter().collect();
+        let mut extra = vec![];
+        stack.push(stack[0]);
+        for x in stack.windows(2) {
+            let Tree(p1x, p1y) = x[0];
+            let Tree(p2x, p2y) = x[1];
+            trees = trees
+                .into_iter()
+                .filter_map(|a| {
+                    let Tree(p_x, p_y) = a;
+                    let vec1 = (p_x - p1x, p_y - p1y);
+                    let vec2 = (p_x - p2x, p_y - p2y);
+                    if (vec1.0 * vec2.0 + vec1.1 * vec2.1).pow(2)
+                        == (vec1.0.pow(2) + vec1.1.pow(2)) * (vec2.0.pow(2) + vec2.1.pow(2))
+                    {
+                        extra.push(a.clone());
+                        // println!("hahaha {:?}", a);
+                        None
+                    } else {
+                        Some(a)
+                    }
+                })
+                .collect();
+        }
+        stack.pop();
+        // println!("extra: {:?}", extra);
         stack
-            .iter()
+            .into_iter()
+            .chain(extra.into_iter())
             .map(|x| vec![x.0 + init.0, x.1 + init.1])
             .collect()
     }
@@ -67,7 +99,6 @@ impl Solution {
 struct Solution;
 
 fn main() {
-    use std::collections::HashSet;
     let res: HashSet<Vec<i32>> = HashSet::from_iter(
         Solution::outer_trees(vec![
             vec![1, 1],
