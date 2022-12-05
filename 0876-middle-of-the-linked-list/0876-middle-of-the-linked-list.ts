66f24c0d236d6a7b1b1f83bc648ee59c924bf972
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-12-05 16:23:40 +0800

    Time: 102 ms (49.12%), Space: 42.8 MB (81.18%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.ts
@@ -0,0 +1,8 @@
+function middleNode(head: ListNode | null): ListNode | null {
+    let fast = head, slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
