5c51738cec2a03f7a89af506b42c9ff0d0ee3abb
猫猫 <overflowcat@gmail.com>
2022-12-19 02:12:24 +0800

    attempt 1

--- rust/daily-temperatures/src/main.rs
+++ rust/daily-temperatures/src/main.rs
@@ -1,23 +1,17 @@
 impl Solution {
     pub fn daily_temperatures(mut temperatures: Vec<i32>) -> Vec<i32> {
-        let mut stack = vec![];
-        for i in 0..temperatures.len() {
+        let (mut stack, n) = (vec![], temperatures.len());
+        for i in 0..n {
             while let Some(&j) = stack.last() {
                 if temperatures[j] < temperatures[i] {
-                    temperatures[j] = (i - j) as i32;
-                    temperatures.pop();
+                    temperatures[j] = (i - stack.pop().unwrap()) as i32;
                 } else {
                     break;
                 }
             }
             stack.push(i);
         }
+        &mut temperatures[n - stack.len() - 1..].fill(0);
         temperatures
     }
-}
-
-struct Solution;
-
-fn main() {
-    println!("Hello, world!");
 }
