a5bd37307a6a9f774860692e5595200102eb9ccc
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-12-20 23:42:51 +0800

    Time: 67 ms (93.24%), Space: 14.4 MB (87.64%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,9 +1,11 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        count = len(rooms)
         visited, stack = {0}, [0]
         while stack:
           for x in rooms[stack.pop()]:
             if x not in visited:
               stack.append(x)
               visited.add(x)
-        return len(rooms) == len(visited)
+          if len(visited) == count: break
+        return len(visited) == count
