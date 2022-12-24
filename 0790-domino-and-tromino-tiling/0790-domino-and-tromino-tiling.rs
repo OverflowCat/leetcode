e3b938ce44a6f6249139c273b24f3df264ae3a63
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-12-24 22:15:39 +0800

    Time: 1 ms (50.00%), Space: 2.1 MB (100.00%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn num_tilings(n: i32) -> i32 {
+        if n < 3 {
+            n
+        } else {
+            let n = n as usize;
+            let mut dp1 = Vec::with_capacity(n + 1);
+            dp1.push(0u64);
+            dp1.push(1);
+            dp1.push(2);
+            let mut dp2 = vec![0, 0, 2u64];
+            for i in 3..=n {
+                dp1.push((dp1[i - 1] + dp1[i - 2] + dp2[i - 1]) % 1000000007);
+                dp2.push((2 * dp1[i - 2] + dp2[i - 1]) % 1000000007);
+            }
+            (dp1[n] % 1000000007) as i32
+        }
+    }
+}
