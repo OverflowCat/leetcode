fc3ebc597bb2dca59ab0ce2d5dc8fca95d2288a5
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2023-01-12 01:18:16 +0800

    Time: 695 ms (87.18%), Space: 49.4 MB (94.87%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,16 +1,16 @@
 class Solution:
     def minTime(self, n: int, e: List[List[int]], ap: List[bool]) -> int:
-        visited = [False] * n
-        tree = [[] for _ in range(n)]
-        for a, b in e: 
-          tree[a].append(b)
-          tree[b].append(a)
-        e = None
-        def f(i: int) -> int:
-          if visited[i]: return 0
-          visited[i] = True
-          res = sum(map(f, tree[i])) # 这题的树也可能不是二叉树！！！！
-          if res == 0 and not ap[i]: return 0
-          return res + 2
-        res = f(0)
-        return res if res == 0 else res - 2
+        visited = [False] * n # 这题的树可能是 [[0,1],[2,3],[1,2]]！
+        tree = [[] for _ in range(n)] # sort 貌似也不管用，为啥有人说可以
+        for a, b in e: # 看评论以前比较水，没有这这些样例
+          tree[a].append(b) # 这题的树可能是 [[0,2],[1,2]]！！
+          tree[b].append(a) # 所以题目保证输入的 a < b 其实并没有什么用！！
+        e = None # 释放内存
+        def f(i: int) -> int: # 不需要经过的情况返回 0
+          if visited[i]: return 0 # 实际上不需要单独建树，
+          visited[i] = True # 累加的过程本身就是从 0 开始按顺序的
+          res = sum(map(f, tree[i])) # 这题的树也可能不是二叉树！！！\U0001f605
+          if res == 0 and not ap[i]: return 0 # 注意只有在子节点和本节点上都
+          return res + 2 # 没有苹果的时候才返回 9，如果本节点上有则要返回 2 ！！
+        ans = f(0) # f 返回值的意思是到达 i 节点所需的时间，所以 f(0) 实际上是从
+        return 0 if ans == 0 else ans - 2 # 一个虚拟节点到 0，会有多余的两步!!!!!
