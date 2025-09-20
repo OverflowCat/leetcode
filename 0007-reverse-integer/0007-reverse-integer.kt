cf997afaffd8192147b36cc2fb53643e8193f3f3
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2025-09-20 19:43:53 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.kt
+++ 0007-reverse-integer/0007-reverse-integer.kt
@@ -3,19 +3,17 @@ import kotlin.math.abs
 class Solution {
     fun reverse(x: Int): Int {
         var res: Int = 0
-        var num = abs(x)
+        var num = x
         while (num != 0) {
-            val last = res
             res *= 10
-            res += num % 10
-            if (res / 10 != last) {
+            val rem = num % 10
+            if (res > Int.MAX_VALUE / 10 || res < Int.MIN_VALUE) {
                 return 0
             }
+            res += rem
             num /= 10
         }
-        if (x < 0) {
-            res = -res
-        }
+
         return res
     }
 }
